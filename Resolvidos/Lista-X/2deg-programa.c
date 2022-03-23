63b5d4a451314ffdca2e56066a69345300576cfc
Uriel Andrade <83730722+UrielAnd@users.noreply.github.com>
2022-03-23 16:39:51 -0300

    Add files via upload

---
+++ Resolvidos/Lista-X/2deg-programa.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>      //BIBLIOTECA//
+#include <string.h>     //BIBLIOTECA//
+int main()                      
+{
+    FILE* arquivo;                                  //Identifidcador de fluxo.//
+    int tam;                                //Declaração da variável auxiliar.//
+    char frase [256];                            //Declaração da string.//
+    
+    arquivo=fopen(criptografa.txt,"w+");                 //Abertura do arquivo.//
+    
+    printf("Frases criptografadas do arquivo.");                //INFO.//
+
+//PROCESSAMENTO_________________________________________________________________________________________________    
+    while(!feof(arquivo))
+    {
+        fscanf(arquivo,"%s", frase);                                //Lê o arquivo.//
+        
+        tam=strlen(frase);
+        for(int repet=0;repet<tam;repet++)                  //Ciclo para descriptografar os textos do arquivo.//
+        {
+            frase[repet]-=1;
+        }
+        fprintf(arquivo,"%s", frase);                       //Armazena no arquivo.//
+    //OUTPUT____________________________________________________________________________________________________    
+        printf("%s", frase);
+    }
+    
+    fclose(arquivo);                    //Fechamento do arquivo.//
+    
+
+    return 0;
+}
